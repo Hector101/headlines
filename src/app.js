20868193861b36e1ab3fc7cc51923a4a49891682
hector101 <hector1014really32@gmail.com>
2017-06-13 06:49:40 +0100

    chore(get-news): add flux architechture and add react components to render api data to user

---
+++ src/app.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import '../public/sass/style.scss';
+import '../public/js/logic';
+import Router from './router/Router';
+
+ReactDOM.render(<Router />, document.getElementById('root'));
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,80 @@
+import React from 'react';
+  // import dependency
+import GoogleLogin from 'react-google-login';
+
+import Auth from '../auth/auth';
+
+/**
+ * Navbar react componet
+ */
+class Navbar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoggedin: Auth.ifLoggedin(),
+    };
+  }
+  conpnonentWillMount() {
+    if (this.state.isLoggedin !== Auth.ifLoggedin()) {
+      this.setState({
+        isLoggedin: Auth.ifLoggedin(),
+      });
+    }
+  }
+
+  /**
+  * call each time login authentication is either
+  successful or faiiled
+  * @param {Object} response
+  */
+  responseGoogle(response) {
+    Auth.signIn(response);
+    Auth.ifLoggedin();
+    window.location.href = '/dashboard';
+  }
+
+  /**
+  * sign user on fire
+  */
+  signOut() {
+    Auth.signOut();
+    Auth.ifLoggedin();
+  }
+  
+  render() {
+    return (
+      <nav className="navbar-fixed #616161 grey darken-2">
+        <div id="navbar" className="nav-wrapper">
+          <a className="brand-logo logo">News Feed</a>
+          <a
+            href="#!"
+            data-activates="slide-out"
+            className="button-collapse"
+          >
+            <i className={'material-icons'}>menu</i></a>
+          <ul id="nav-mobile" className="right">
+            <li>
+              {
+              !Auth.ifLoggedin() ? (<GoogleLogin
+                clientId={'280387982886-h6rndqluquvvgujtos24h4vbkjv7rq6l.apps.googleusercontent.com'}
+                buttonText="Sign in"
+                onSuccess={this.responseGoogle}
+                onFailure={this.responseGoogle}
+                tag="a"
+                className="login"
+              ><span className="white-text">Sign in</span>
+                <i className="material-icons left">arrow_forward</i>
+              </GoogleLogin>) : (<a href="/" onClick={this.signOut} >
+              Sign out<i className="material-icons left">arrow_back</i></a>)
+            }
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
+  }
+
+}
+
+export default Navbar;
+
