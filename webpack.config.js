688a5adfe773c6399ac6be90483b9ad12e60c21e
hector101 <hector1014really32@gmail.com>
2017-06-13 05:26:37 +0100

    chhore(setup-webpack): Configure the webpack for project

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackHtmlPlugin = require('html-webpack-plugin');
+
+const hotModuleReplacementPlugin = new webpack.HotModuleReplacementPlugin();
+const htmlWebpackHtmlPlugin = new HtmlWebpackHtmlPlugin({
+  template: './index.html',
+  inject: 'body',
+});
+
+// setup webpack configuration
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.min.js',
+    publicPath: '/',
+  },
+  devServer: {
+    compress: true,
+    hot: true,
+    port: 3000,
+    historyApiFallback: true,
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$|\.jsx$/,
+        use: ['babel-loader'],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$|\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.jpg$|\.png$|\.svg$/,
+        use: ['file-loder'],
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [hotModuleReplacementPlugin, htmlWebpackHtmlPlugin],
+};
