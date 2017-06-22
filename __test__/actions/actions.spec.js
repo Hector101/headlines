ce7b16f361f4711e3b87d9ed2b6da47d5ebb336f
hector101 <hector1014really32@gmail.com>
2017-06-22 16:20:26 +0100

    chore(refactor-test): refactor test and add new tests

--- __test__/actions/actions.spec.js
+++ __test__/actions/actions.spec.js
@@ -2,8 +2,10 @@ import sinon from 'sinon';
 import Dispatcher from '../../src/flux/dispatcher/dispatcher';
 import Actions from '../../src/flux/actions/Actions';
 import Api from '../../src/api/api';
+import store from '../../src/flux/store/store';
 import mockSources from '../../src/__mock__/sources.json';
 import mockArticles from '../../src/__mock__/articles.json';
+import Auth from '../../src/auth/auth';
 
 jest.mock('../../src/api/api', () => ({
   getSources: () => Promise.resolve({ sources: mockSources.sources }),
@@ -23,30 +25,35 @@ describe('When Actions', () => {
     DispatcherMock.restore();
   });
 
-  describe('Creator Method,', () => {
-    it('getSources is called, ApiGetSources to be called', () => {
-      Actions.getSources();
-      expect(ApiGetSources.called).toBeTruthy();
+  describe('Action,', () => {
+    describe('get sources and articles method', () => {
+      it('getSources is called, ApiGetSources to be called', () => {
+        Actions.getSources();
+        expect(ApiGetSources.called).toBeTruthy();
+      });
+      it('getArticles is called with two parameter, ApiGetArticles to be called', () => {
+        Actions.getArticles('cnn', 'top');
+        expect(ApiGetArticles.called).toBeTruthy();
+      });
+      it('getArticles is called with one parameter, ApiGetArticles to be called', () => {
+        Actions.getArticles('cnn');
+        expect(ApiGetArticles.called).toBeTruthy();
+      });
+      it('selectedSource is called, Dispatcher is called', () => {
+        Actions.selectedSource('cnn');
+        expect(selectedSourceMock.called).toBe(true);
+        expect(DispatcherMock.called).toBe(true);
+      });
     });
-    it('getArticles is called with two parameter, ApiGetArticles to be called', () => {
-      Actions.getArticles('cnn', 'top');
-      expect(ApiGetArticles.called).toBeTruthy();
-    });
-    it('getArticles is called with one parameter, ApiGetArticles to be called', () => {
-      Actions.getArticles('cnn');
-      expect(ApiGetArticles.called).toBeTruthy();
-    });
-    it('selectedSource is called, Dispatcher is called', () => {
-      Actions.selectedSource('cnn');
-      expect(selectedSourceMock.called).toBe(true);
-      expect(DispatcherMock.called).toBe(true);
-    });
-    it('getAuth to be called with a string parameter', () => {
-      Actions.getAuth(true);
-      expect(selectedSourceMock.called).toBe(true);
-    });
-    it('getAuth to be called with a string parameter', () => {
-      Actions.getSources();
+    describe('for authentication', () => {
+      it('getAuth to be called with a string parameter', () => {
+        Actions.getAuth(true);
+        expect(selectedSourceMock.called).toBe(true);
+      });
+      it('store should update and return "true" which is the dispatched payload ', () => {
+        Actions.getAuth(true);
+        expect(store.getAuth()).toBe(true);
+      });
     });
   });
 });
--- __test__/components/mainbar.spec.js
+++ __test__/components/mainbar.spec.js
@@ -5,18 +5,20 @@ import MainBar from '../../src/components/Mainbar';
 import articles from '../../src/__mock__/articles.json';
 import updatedArticles from '../../src/__mock__/updatedArticles.json';
 
-describe('Mainbar component', () => {
+describe('Mainbar', () => {
   const wrapper = shallow(<MainBar articles={articles.articles} newsType={'techcrunch'} />);
   const spyComponentWillRecieveProps = sinon.spy(MainBar.prototype, 'componentWillReceiveProps');
 
-  it('componentWillReceiveProps should not called until props updates', () => {
-    expect(spyComponentWillRecieveProps.called).toBe(false);
-  });
-  it('componentWillReceiveProps should be called when prop changes', () => {
-    wrapper.setProps({
-      articles: updatedArticles.articles,
-      newsType: 'cnn',
+  describe('componentWillReceiveProps', () => {
+    it('should not called until props updates', () => {
+      expect(spyComponentWillRecieveProps.called).toBe(false);
     });
-    expect(spyComponentWillRecieveProps.called).toBe(true);
+    it('should be called when prop changes', () => {
+      wrapper.setProps({
+        articles: updatedArticles.articles,
+        newsType: 'cnn',
+      });
+      expect(spyComponentWillRecieveProps.called).toBe(true);
+    });
   });
 });
