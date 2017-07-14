8ea6c02181b7220bc46bbe0bee077c0202bf1f81
hector101 <hector1014really32@gmail.com>
2017-07-14 08:18:14 +0100

    add comments to server.js file

--- server.js
+++ server.js
@@ -2,12 +2,29 @@ const express = require('express');
 const path = require('path');
 const history = require('connect-history-api-fallback');
 
+/**
+ * set port number for server connection
+ */
 const port = process.env.PORT || 8080;
+
+/**
+ * create instance of express
+ */
 const app = express();
+
+/**
+ * add history middleware
+ */
 app.use(history());
 
+/**
+ * setup root directory to server static asssets
+ */
 app.use(express.static(path.resolve(__dirname)));
 
+/**
+ * start socket connection
+ */
 app.listen(port, () => {
-  console.log('Server Started on port 8000');
+  console.log('Server Started on port 8080');
 });
